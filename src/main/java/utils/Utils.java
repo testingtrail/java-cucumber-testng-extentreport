2a47950c73176692c4e7bca937619a13d0a5d2c8
Jorge Quiros Torres <40399705+jorgeautomation@users.noreply.github.com>
2019-02-27 15:23:21 -0600

    Update Utils.java
    adding more methods to utils class

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -11,6 +11,11 @@ import org.openqa.selenium.WebDriver;
 
 public class Utils {
 
+     /*************************************************************
+     Description: Checks if an element exists using locator
+     Paramaters: webdriver, the locator
+     Return: boolean
+     ***********************************************************/
     public static boolean existsElement (WebDriver driver, By locator){
             try {
                 driver.findElement(locator);
@@ -18,6 +23,44 @@ public class Utils {
                return false;
             }
         return true;
+    }
+ 
+    /*************************************************************
+     Description: Checks if an element is visible or not, so for instance an element that is in the DOM
+        but may be or not present depending on conditions
+     Paramaters: webdriver, time in seconds to wait, locator
+     Return: element if found, if not returns null
+     ***********************************************************/
+    public static WebElement visibleOrNot(WebDriver driver, int timeToWait, By locator) {
+        WebElement element = null;
+        try {
+            element = new FluentWait<>(driver).
+                    withTimeout(timeToWait, TimeUnit.SECONDS).
+                    pollingEvery(1, TimeUnit.SECONDS).
+                    ignoring(NotFoundException.class).ignoring(NoSuchElementException.class).
+                    until(visibilityOfElementLocated(locator));
+        } catch (TimeoutException ex) {}
+        return element;
+    }
+ 
+    /*************************************************************
+     Description: Clicks an element using Javascript
+     Paramaters: driver, webelement to click
+     Return: NA
+     ***********************************************************/
+    public static void clickElementThroughJavaScript(WebDriver driver, WebElement element){
+        //To Click on elements which may not be visible on screen
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+ 
+     /*************************************************************
+     Description: Attach an screenshot, with allure attachment tag
+     Paramaters: webdriver
+     Return: NA
+     ***********************************************************/
+    @Attachment( type = "image/png")
+    public static  byte[] screenShot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
 }
